57ced5355d8fdcd0981df182612184e51060a99a
Deveshnagar87 <nagardevesh2004@gmail.com>
2024-09-09 17:12:45 +0530

    Update config.py

--- config.py
+++ config.py
@@ -30,8 +30,8 @@ DB_NAME = os.environ.get("DATABASE_NAME", "nagardeveshjpr")
 # check my discription to help by using my refer link of shareus.io
 # 
 
-SHORTLINK_URL = os.environ.get("SHORTLINK_URL", "inshorturl.com")
-SHORTLINK_API = os.environ.get("SHORTLINK_API", "193cde54f7953e515b45898710f8b2035b806605")
+SHORTLINK_URL = os.environ.get("SHORTLINK_URL", "modijiurl.com")
+SHORTLINK_API = os.environ.get("SHORTLINK_API", "0e8c51186d4d8749699c8356562a6755aae2656a")
 VERIFY_EXPIRE = int(os.environ.get('VERIFY_EXPIRE', 86400)) # Add time in seconds
 IS_VERIFY = os.environ.get("IS_VERIFY", "True")
 TUT_VID = os.environ.get("TUT_VID", "https://t.me/link_how_to_download/13") # shareus ka tut_vid he 
