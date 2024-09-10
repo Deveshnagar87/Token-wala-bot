9a88e4d8e602746441caab5d3c1b07ff40b204ea
Deveshnagar87 <nagardevesh2004@gmail.com>
2024-09-10 19:14:19 +0530

    Update config.py

--- config.py
+++ config.py
@@ -24,7 +24,7 @@ PORT = os.environ.get("PORT", "8080")
 #Database 
 #Database [https://youtu.be/qFB0cFqiyOM?si=fVicsCcRSmpuja1A]
 DB_URI = os.environ.get("DATABASE_URL", "mongodb+srv://nagardeveshjpr:vDBSHxlkeAfVAWs8@cluster0.slvhm.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
-DB_NAME = os.environ.get("DATABASE_NAME", "tedhemedhe")
+DB_NAME = os.environ.get("DATABASE_NAME", "nagardeveshjpr")
 
 #Shortner (token system) 
 # check my discription to help by using my refer link of shareus.io
